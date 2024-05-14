be9b151aeb12f45941b409b1c26eca2997bf1520
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-14 14:38:16 +0000

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// Desafio: criar uma rota [GET] /oi que retorna "Olá, mundo!"
+app.get('/oi', function (req, res) {
+  res.send('Olá, mundo!')
+})
+
 app.listen(3000)
