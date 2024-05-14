6c0e2e02cfcbc62c89b6941920324728ba5aed41
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-14 18:48:31 +0000

    Melhoria na visualização da lista de itens

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const itens = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
 // Endpoint de Read All [GET] /item
 app.get('/item', function (req, res) {
-  res.send(itens)
+  res.send(itens.filter(Boolean))
 })
 
 // Endpoint de Read By ID [GET] /item/:id
